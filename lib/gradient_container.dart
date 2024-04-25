578360f308295d08ad9b9b7ba65c4eca22675405
Furkan Şen <furkansen202@gmail.com>
2024-04-26 00:48:38 +0300

    initial commit

---
+++ lib/gradient_container.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [color1, color2],
+              begin: startAlignment,
+              end: endAlignment)),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/quiz-logo.png',
+              width: 300,
+            ),
+            const SizedBox(height: 80),
+            const Text(
+              'Learn Flutter the fun way!',
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
+            const SizedBox(height: 30),
+            OutlinedButton(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.white,
+              ),
+              child: const Text('Start Quiz'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
