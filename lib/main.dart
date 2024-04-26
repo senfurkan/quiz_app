eaba5d0e3b6c6087b831341cb863c14fa7764666
Furkan Şen <furkansen202@gmail.com>
2024-04-27 00:34:55 +0300

    The quiz.dart component is rendered in the main.dart file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-import 'package:adv_basics/gradient_container.dart';
 import 'package:flutter/material.dart';
 
+import 'package:adv_basics/quiz.dart';
+
 void main() {
-  runApp(const MaterialApp(
-      home: Scaffold(
-          body: GradientContainer(
-    Color.fromARGB(255, 33, 5, 109),
-    Color.fromARGB(255, 68, 21, 149),
-  ))));
+  runApp(const Quiz());
 }
