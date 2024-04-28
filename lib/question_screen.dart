0222665fc85ea611e95a92df49aea96483c615c8
Furkan Şen <furkansen202@gmail.com>
2024-04-28 18:34:48 +0300

    By using the spread operator and map function on the questions screen, the questions were displayed dynamically without code repetition. centered horizontally.

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,3 +1,5 @@
+import 'package:adv_basics/answer_button.dart';
+import 'package:adv_basics/data/questions.dart';
 import 'package:flutter/material.dart';
 
 class QuestionScreen extends StatefulWidget {
@@ -12,6 +14,32 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(context) {
-    return const Text('QuestionScreen');
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.answers.map((item) {
+              return AnswerButton(answerText: item, onTap: () {});
+            })
+          ],
+        ),
+      ),
+    );
   }
 }
